8e03df6f1e647c6ceedd0d5822b6512e0fc7b67d
hgrafa <hugorafell@gmail.com>
2023-01-13 10:50:52 -0300

    chore: refactoring to Oriented Object Programming

---
+++ ByteBank1/View/MenuView.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ByteBank1.View {
+    public class MenuView {
+
+        public static void ShowMenu() {
+            Console.WriteLine("1 - Inserir novo usuário");
+            Console.WriteLine("2 - Deletar um usuário");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("4 - Detalhes de um usuário");
+            Console.WriteLine("5 - Quantia armazenada no banco");
+            Console.WriteLine("6 - Manipular a conta");
+            Console.WriteLine("0 - Para sair do programa");
+            Console.Write("Digite a opção desejada: ");
+        }
+
+    }
+}
